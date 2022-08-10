2457094484ccdbd4f2f09c020149890f7ff3098a
JaeAiKay <78583537+JaeAiKay@users.noreply.github.com>
2022-08-10 22:49:47 +0700

    first commit

---
+++ src/MGame.java
@@ -0,0 +1,28 @@
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MGame {
+    private int roundCnt;
+
+    private ArrayList<Player> players;
+    private ArrayList<Die> dice;
+    private Board board;
+
+    public MGame(ArrayList<Player> players){
+        roundCnt = 0;
+        this.players = players;
+    }
+
+    public void playGame(int n){
+        while (roundCnt < n){
+            playRound();
+        }
+    }
+
+    private void playRound(){
+        for(Player player : players){
+            player.takeTurn();
+        }
+    }
+}
